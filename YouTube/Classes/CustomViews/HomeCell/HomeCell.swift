72aeb6cd78bb04ea29a7dc04307d217e08edc702
Duy Tang <duytangdhv@gmail.com>
2016-08-10 11:57:45 +0700

    Add CategoryCell, HomeCell, DownloadImage by Haneke

--- YouTube/Classes/CustomViews/HomeCell/HomeCell.swift
+++ YouTube/Classes/CustomViews/HomeCell/HomeCell.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class HomeCell: BaseTableViewCell {
 
-    @IBOutlet weak private var thumbailVideo: UIImageView!
+    @IBOutlet weak private var thumbnailVideo: UIImageView!
     @IBOutlet weak private var nameVideoLabel: UILabel!
+    @IBOutlet weak private var nameChannelLabel: UILabel!
+    @IBOutlet weak private var numberViewLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         setUpUI()
@@ -22,12 +24,14 @@ class HomeCell: BaseTableViewCell {
 
     }
     // MARK:- Configure Cell
-    func configureCell() {
-        self.nameVideoLabel.text = "Video"
+    func configureCell(video: Video) {
+        thumbnailVideo.downloadImage(video.thumbnail)
+        self.nameVideoLabel.text = video.title
+        self.nameChannelLabel.text = video.channelTitle
+        self.numberViewLabel.text = video.viewCount
     }
     // MARk:- Set Up UI
     override func setUpUI() {
-        configureCell()
     }
 
 }
