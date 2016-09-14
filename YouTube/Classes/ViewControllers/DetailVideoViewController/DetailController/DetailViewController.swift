d2be8babe98a03239f06035c24ed94f38346b2e6
Duy Tang <duytangdhv@gmail.com>
2016-09-14 17:03:19 +0700

    Add pageVC

---
+++ YouTube/Classes/ViewControllers/DetailVideoViewController/DetailController/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  YouTube
+//
+//  Created by Duy Tang on 9/14/16.
+//  Copyright © 2016 Duy Tang. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
